32d3ad7af18613c0f9ea99a0c6ad92e53f4a775f
Finn-Perera <finnperera@me.com>
2024-12-11 14:35:57 +0000

    Included load more for comments & validation improved

--- app/Livewire/CommentSection.php
+++ app/Livewire/CommentSection.php
@@ -13,20 +13,25 @@ class CommentSection extends Component
     public $commentableId;
     public $commentableType;
     public $comments;
+    public $loadedCommentsCount = 10;
+    public $totalCommentsCount = 0;
+    public $parent_id = null;
 
     public function submitComment() {
         
         if (!auth()->check()) {
             session()->put('url.intended', url()->previous());
             session()->put('content', $this->commentContent);
-
+            session()->flash('message', 'You must be logged in to comment.');
+    
             return redirect()->route('login');
         }
 
         $this->validate([
-            'commentContent' => 'required',
-            'commentableId' => 'required',
-            'commentableType' => 'required',
+            'commentContent' => 'required|string|max:1000',
+            'parent_id' => 'nullable|integer|exists:comments,id',
+            'commentableType' => 'required|string',
+            'commentableId' => 'required|integer',
         ]);
 
         $comment = Comment::create([
@@ -40,13 +45,28 @@ class CommentSection extends Component
         $this->reset('commentContent');
         $this->loadComments();
     }
-
+    
     public function loadComments() {
+        $this->updateTotalComments();
+
         $this->comments = Comment::where('commentable_type',$this->commentableType)
             ->where('commentable_id', $this->commentableId)
             ->whereNull('parent_id')
             ->with('replies')
+            ->take($this->loadedCommentsCount)
             ->get();
+    }
+
+    public function loadMore() {
+        $this->loadedCommentsCount += 10;
+        $this->loadComments();
+    }
+
+    public function updateTotalComments() {
+        $this->totalCommentsCount = Comment::where('commentable_type',$this->commentableType)
+            ->where('commentable_id', $this->commentableId)
+            ->whereNull('parent_id')
+            ->count();
     }
 
     public function mount($commentable) {
--- resources/views/livewire/comment-section.blade.php
+++ resources/views/livewire/comment-section.blade.php
@@ -4,6 +4,7 @@
         <input wire:model="commentContent" type="text" name="content" placeholder="Add a comment..." required></input>    
         <input type="hidden" wire:model="commentableType" value="{{ $commentableType }}">
         <input type="hidden" wire:model="commentableId" value="{{ $commentableId }}">
+        <input type="hidden" wire:model="parent_id" value="{{ $parent_id }}">
 
         @error('commentContent') 
             <p class="error">{{ $message }}</p>
@@ -15,10 +16,13 @@
     @if (session()->has('message'))
         <p>{{ session('message') }}</p>
     @endif
-
     @if ($comments->isEmpty())
         <p>No comments yet!</p>
     @else
         @include('comments.partials.display-comments', ['comments' => $comments])
+    @endif
+
+    @if ($loadedCommentsCount < $totalCommentsCount)
+        <button wire:click="loadMore" class="btn btn-primary">Show More</button>
     @endif
 </div>
